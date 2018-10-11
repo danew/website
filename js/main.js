7b408f5b1143300c3a49eb0cdc279012e37ffcc9
Dane Wilson <git@danewilson.mefork@danewilson.me>
2018-10-11 18:53:27 -0400

    Verison 1.0

--- js/main.js
+++ js/main.js
@@ -1,35 +1,39 @@
-var DOM = {};
+{
+  var DOM = {};
 
-function init() {
-  DOM.body = document.body;
-  DOM.background = document.querySelector('.background');
-  DOM.width = window.screen.width;
-  // add resize listener to remove and change width value
-  background();
-}
+  function init() {
+    DOM.body = document.body;
+    DOM.background = document.querySelector('.background');
+    DOM.width = window.screen.width;
+  }
 
-function background() {
-  DOM.body.addEventListener('mousemove', throttle(function (event) {
-    var halfScreen = DOM.width/2;
-    var pos = halfScreen - event.screenX;
-    var percentage = ((halfScreen - (pos / 20))/DOM.width * 100).toFixed(2) + '%';
-    DOM.background.style.width = percentage;
-  }), 100);
-}
+  function background() {
+    DOM.body.addEventListener('mousemove', throttle(function (event) {
+      var halfScreen = DOM.width/2;
+      var pos = halfScreen - event.screenX;
+      var percentage = ((halfScreen - (pos / 20))/DOM.width * 100).toFixed(2) + '%';
+      DOM.background.style.width = percentage;
+    }), 100);
+  }
 
-function throttle(func, limit) {
-  var inThrottle;
-  return function() {
-    var args = arguments;
-    var context = this;
-    if (!inThrottle) {
-      func.apply(context, args);
-      inThrottle = true;
-      setTimeout(function() {
-        inThrottle = false;
-      }, limit);
+  function throttle(func, limit) {
+    var inThrottle;
+    return function() {
+      var args = arguments;
+      var context = this;
+      if (!inThrottle) {
+        func.apply(context, args);
+        inThrottle = true;
+        setTimeout(function() {
+          inThrottle = false;
+        }, limit);
+      }
     }
   }
-}
 
-document.addEventListener('DOMContentLoaded', init);
+  window.addEventListener('resize', init);
+  document.addEventListener('DOMContentLoaded', function () {
+    init();
+    background();
+  });
+}
